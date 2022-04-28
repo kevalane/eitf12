80ef8f518e6b3166e223c4b08fde8a0c9d921bdb
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-04-28 21:01:48 +0200

    got i2c to work

--- main.c
+++ main.c
@@ -5,42 +5,92 @@
  * Author : ke5144ra-s
  */ 
 
-#define F_CPU 8000000UL
+#define F_CPU		8000000UL
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <compat/twi.h>
 
-#define LED	(1 << PA0)	// 0b00000001
+#define LED			(1 << PA0)	// 0b00000001
 
 // Keyboard
 #define KB_DATA		PINA
-#define KB_A (1 << PA1) // 0b00000010
-#define KB_B (1 << PA2) // 0b00000100
-#define KB_C (1 << PA3) // 0b00001000
-#define KB_D (1 << PA4) // 0b00010000
-#define KB_AVAIL (1 << PD2)		// INT0
+#define KB_A		(1 << PA1) // 0b00000010
+#define KB_B		(1 << PA2) // 0b00000100
+#define KB_C		(1 << PA3) // 0b00001000
+#define KB_D		(1 << PA4) // 0b00010000
+#define KB_AVAIL	(1 << PD2) // INT0
 
 // FOR OUR RTC
-#define SLA_R   0b11010001                             //address for ds1307, MAYBE CHANGE
-#define SLA_W   0b11010000
-#define sec_reg 0x00
-#define min_reg 0x01
-#define hour_reg 0x02
+#define SCL_CLOCK	100000UL
+#define HOUR_24		0
+#define HOUR_12		1
+#define SLA_R		0b11011111
+#define SLA_W		0b11011110
+#define SEC_REG		0x00
+#define MIN_REG		0x01
+#define HOUR_REG	0x02
+
+void rtc_read_clock(void);
+void rtc_start_clock(void);
+void rtc_set_clock_format(uint8_t format);
+void rtc_set_time(); // TODO
+uint8_t serialize_seconds(int raw_seconds);
+uint8_t serialize_minutes(int raw_minutes);
+uint8_t serialize_hours(int raw_hours);
 
 void twi_init(void);
-void twi_start(void);
+unsigned char twi_start(unsigned char address);
+void twi_start_wait(unsigned char address);
+unsigned char twi_rep_start(unsigned char address);
 void twi_stop(void);
-void twi_write(uint8_t data);
-uint8_t twi_read_ack(void);
-uint8_t twi_read_nack(void);
-uint8_t twi_get_status(void);
+unsigned char twi_write(unsigned char data);
+unsigned char twi_read_ack(void);
+unsigned char twi_read_nack(void);
+uint8_t read_specific_address(uint8_t address);
 
 volatile uint8_t kb_val;
 
 ISR (INT0_vect)          //External interrupt_zero ISR
 {
 	kb_val =  ((KB_DATA & (KB_A | KB_B | KB_C | KB_D)) >> 1);
+	switch (kb_val) {
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		case 10:
+			break;
+		case 11:
+			break;
+		case 12:
+			break;
+		case 13:
+			break;
+		case 14:
+			break;
+		case 15:
+			break;
+		default:
+			break;
+	}
 }
 
 int main(void)
@@ -58,110 +108,141 @@ int main(void)
 	
     while (1) 
     {
-		// let's read it
-		//twi_start();
-		data = read_specific_address(0);
-		//twi_stop();
-		
 		PORTA |= LED;
-		_delay_ms(500);
+		//_delay_ms(500);
 		PORTA &= ~LED;
-		_delay_ms(500);
+		//_delay_ms(500);
+		//rtc_set_clock_format(HOUR_24);
+		rtc_read_clock();
     }
 }
 
-void twi_init(void) {
-	TWSR = 0x00; // removes all initial values (no status on init)
-	/*
-	SCL freq = CPU Clock freq/ (16 + 2*TWBR*4^TWPS)
-	TWBR = Value of the TWI Bit Rate Register
-	TWPS = Value of the prescaler bits in the TWI Status Register
-	Sets scl to 400kHz?
-	200kHz
-	*/
-	TWBR = 0x0c; // adds 0b00001100
-	TWCR = (1 << TWEN); // activates the twi interface by setting 2nd bit
+void rtc_set_clock_format(uint8_t format) {
+	uint8_t currentData = read_specific_address(HOUR_REG);
+	twi_start(SLA_W);
+	twi_write(HOUR_REG);
+	uint8_t dataToSend = currentData;
+	if (format) {
+		// 12 hour
+		dataToSend |= (HOUR_12 << 6);
+	} else {
+		// 24 hour
+		dataToSend |= (HOUR_24 << 6);
+	}
+	twi_write(dataToSend);
+	twi_stop();
 }
 
-void twi_start(void) {
-	TWCR |= (1 << TWINT); // set interrupt flag
-	TWCR |= (1 << TWSTA); // become master
-	TWCR |= (1 << TWEN); // like in init, starts interface
-	while ((TWCR & (1 << TWINT)) == 0) {}; // if TWINT is not set, get stuck
+void rtc_start_clock(void) {
+	twi_start(SLA_W);
+	twi_write(0x00);
+	twi_write(0b10000000);
+	twi_stop();
+}
+
+void rtc_read_clock(void) {
+	int raw_seconds;
+	int raw_minutes;
+	int raw_hours;
+	twi_start(SLA_W);
+	twi_write(0x00);
+	twi_start(SLA_R);
+	raw_seconds = twi_read_ack();
+	raw_minutes = twi_read_ack();
+	raw_hours = twi_read_nack();
+	uint8_t seconds = serialize_seconds(raw_seconds);
+	uint8_t minutes = serialize_minutes(raw_minutes);
+	uint8_t hours = serialize_hours(raw_hours);
+	twi_stop();
+}
+
+uint8_t serialize_seconds(int raw_seconds) {
+	uint8_t ones = (uint8_t) (raw_seconds & 0b00001111);
+	uint8_t tens = (uint8_t) (raw_seconds & 0b01110000);
+	tens = (tens >> 4);
+	return ones + tens*10;
+}
+
+uint8_t serialize_minutes(int raw_minutes) {
+	uint8_t ones = (uint8_t) (raw_minutes & 0b00001111);
+	uint8_t tens = (uint8_t) (raw_minutes & 0b01110000);
+	tens = (tens >> 4);
+	return ones + tens*10;
+}
+
+uint8_t serialize_hours(int raw_hours) {
+	uint8_t ones = (uint8_t) (raw_hours & 0b00001111);
+	uint8_t tens = (uint8_t) (raw_hours & 0b00110000);
+	tens = (tens >> 4);
+	return ones + tens*10;
+}
+
+void twi_init(void) {
+	TWSR = 0;
+	TWBR = ((F_CPU/SCL_CLOCK)-16)/2;
+}
+
+unsigned char twi_start(unsigned char address) {
+	uint8_t twst;
+
+	TWCR = 0;
+	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN); // set interrupt flag - become master - like in init, starts interface
+	while (!(TWCR & (1<<TWINT))); // if TWINT is not set, get stuck
+	twst = TW_STATUS & 0xf8;
+	if ((twst != TW_START)) return 1;
+
+	// send device address
+	TWDR = address;
+	TWCR = (1 << TWINT) | (1 << TWEN);
+
+	// wait for transmission
+	while (!(TWCR & (1 << TWINT)));
+
+	// check value of TWI Status Register. Mask prescaler bits.
+	twst = TW_STATUS & 0xF8;
+	if ( (twst != TW_MT_SLA_ACK) && (twst != TW_MR_SLA_ACK) ) return 1;
+
+	return 0;
+}
+
+unsigned char twi_rep_start(unsigned char address) {
+	return twi_start(address);
 }
 
 void twi_stop(void) {
-	TWCR |= (1 << TWINT); // we are done, interrupt flag
-	TWCR |= (1 << TWSTO); // stop bit generates stop
-	TWCR |= (1 << TWEN); // activates interface like before
+	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);  // we are done, interrupt flag, stop bit generates stop, activates interface like before
+	// wait until stop condition is executed and bus released
+	while(TWCR & (1<<TWSTO));
 }
 
-void twi_write(uint8_t data) {
+unsigned char twi_write(unsigned char data) {
+	uint8_t twst;
 	TWDR = data; // data registry containing next byte to transmit, obvious
-	TWCR |= (1 << TWINT); // interrupt flag that we are done
-	TWCR |= (1 << TWEN); // reactivates interface
-	while((TWCR & (1 << TWINT)) == 0) {}; // like before, if TWINT is not set, get stuck because it means we are not done
+	TWCR = (1<<TWINT) | (1<<TWEN); // interrupt flag that we are done -  reactivates interface
+	while(!(TWCR & (1<<TWINT))); // like before, if TWINT is not set, get stuck because it means we are not done
+	twst = TW_STATUS & 0xF8;
+	if( twst != TW_MT_DATA_ACK) return 1;
+	return 0;
 }
 
-uint8_t twi_read_ack(void) {
-	TWCR |= (1 << TWINT); // interrupt flag, we are done
-	TWCR |= (1 << TWEN); // enable again
-	TWCR |= (1 << TWEA); // setting to one generates an ACK pulse on twi bus
-	while ((TWCR & (1 << TWINT)) == 0) {}; // wait for interrupt!
-	return TWDR; // return the data on twi bus (is this the ACK?)	
+unsigned char twi_read_ack(void) {
+	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWEA); // interrupt flag, we are done - enable again - setting to one generates an ack pulse on twi bus
+	while(!(TWCR & (1<<TWINT))); // wait for interrupt!
+	return TWDR; // return the data on twi bus (is this the ACK?)
 }
 
-uint8_t twi_read_nack(void) {
-	TWCR |= (1 << TWINT); // interrupt flag, done
-	TWCR |= (1 << TWEN); // enable again
-	while ((TWCR & (1 << TWINT)) == 0) {}; // wait for interrupt to be set
-	return TWDR; // have not generated ack, maybe this is empty now?
+unsigned char twi_read_nack(void) {
+	TWCR = (1<<TWINT) | (1<<TWEN);// interrupt flag, done - enable again
+	while(!(TWCR & (1<<TWINT)));// wait for interrupt to be set
+	return TWDR;// have not generated ack, maybe this is empty now?
 }
 
-uint8_t twi_get_status(void) {
-	uint8_t status;
-	status = TWSR & 0b11111000; // we don't want the reserved bits, only bits 7-3!!
-	return status;
-}
-
-int read_rtc(void) {
-	int recv_data;
-	TWCR=(1<<TWINT)|(1<<TWEN);    // Clear TWI interrupt flag,Enable TWI
-	while (!(TWCR & (1<<TWINT))); // Wait till complete TWDR byte transmitted
-	recv_data=TWDR;	
-	return recv_data;
-}
-
-void twi_send_address_write() {
-	TWDR = SLA_W;
-	TWCR = (1<<TWINT) | (1<<TWEN);     // Load SLA_W into TWDR Register. Clear TWINT bit in TWCR to start transmission of address
-	while (!(TWCR & (1<<TWINT)));      //Wait for TWINT Flag set. This indicates that the SLA+W has been transmitted, and ACK/NACK has been received
-	//return 0;
-}
-
-void twi_send_address_data(uint8_t data) {
-	TWDR = data;
-	TWCR = (1<<TWINT) | (1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));
-	//return 0;
-}
-
-void twi_send_address_read() {
-	TWDR = SLA_R;
-	TWCR = (1<<TWINT) | (1<<TWEN); 
-	while (!(TWCR & (1<<TWINT)));
-	//return 0;
-}
-
-int read_specific_address(uint8_t address) {
-	int returnData;
-	twi_init();
-	twi_start();
-	twi_send_address_write();
-	twi_send_address_data(address);
-	twi_start();
-	twi_send_address_read();
-	returnData = read_rtc();
+uint8_t read_specific_address(uint8_t address) {
+	uint8_t data;
+	twi_start(SLA_W);
+	twi_write(address);
+	twi_start(SLA_R);
+	data = twi_read_nack();
 	twi_stop();
-	return returnData;
+	return data;
 }
